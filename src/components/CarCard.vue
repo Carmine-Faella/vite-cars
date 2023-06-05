9b301a2a0965de547814ca28c400195350715f7b
Camine123456 <carminefaella5@gmail.com>
2023-06-05 17:17:05 +0200

    Fine implementazione show

--- src/components/CarCard.vue
+++ src/components/CarCard.vue
@@ -1,11 +1,30 @@
 <script>
 
+import { store } from '../store.js';
+
 export default {
-    name: 'CarCard'
+    name: 'CarCard',
+    data(){
+        return{
+            store
+        }
+    },
+    props:['car'],
 }
 
 </script>
 
 <template>
-   
+   <div class="card bg-black text-white text-center">
+        <div class="card-body card-wrapper">
+            <h5 class="card-title py-3">{{car.model}}</h5>
+            <p class="card-text">{{car.price}}</p>
+            <p class="card-text">{{car.cc}}</p>
+            <p class="badge text-bg-primary">{{car.optionals?.name}}</p>
+            <router-link :to="{name: 'single-cars', params: {id:car.id}}" class="btn btn-primary">
+                Vedi card completa
+            </router-link>
+
+        </div>
+    </div>
 </template>
--- src/pages/SingleCar.vue
+++ src/pages/SingleCar.vue
@@ -1,11 +1,41 @@
 <script>
+    import axios from 'axios';
+    import { store } from '../store.js';
 
-export default {
-    name: 'SingleCar'
-}
+    export default {
+        name: 'SingleCar',
+        data() {
+            return {
+                store,
+                cars:null
+            }
+        },
+        mounted() {
+            const id = this.$route.params.id;
+            
+            axios.get(`${this.store.url}/api/cars/${id}`)
+            .then(response => {
+                if(response.data.success == true){
+                    this.cars = response.data.results;
+                }else{
+                    this.$router.push({name:'NotFound'});
+                }
+                console.log(response)
+            });
 
+        }
+    }
 </script>
 
 <template>
-   
+
+    <div v-if="cars" class="text-center">
+        <h5 class="">{{cars.model}}</h5>
+        <div class="badge text-bg-primary">{{cars.optionals?.name}}</div>
+        <p>{{cars.brand}}</p>
+        <p>{{cars.price}}</p>
+        <p>{{cars.cc}}</p>
+        <p>{{cars.year_release}}</p>
+    </div>
+
 </template>
